8af2143a8d30fe309f90d56bd6d5ff9eee7b739b
jjhlzn <jinjunhang@hotmail.com>
2014-08-27 17:26:30 +0800

    更新

--- ticketsystem_report.py
+++ ticketsystem_report.py
@@ -3,4 +3,10 @@ from  database import *
 get_connection()
 
 def dailyreport_network_use_orderdate(date):
-    
+    """
+     select DDate, COUNT(*) as order_number, SUM(DDjNumber) as people_number, SUM(DAmount) as total_money from v_tbdTravelOk a where DDate >= '2013-1-1' and Flag in (1) and
+exists(select * from tbdGroupType b where a.DGroupType = b.DName and a.DGroupTypeAssort = b.sType and DGroupRoomType = '网络用房') group by DDate
+order by DDate
+    :param date:
+    :return:
+    """
